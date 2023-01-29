7b34cd9f264e5fa8b82734d1fb8b1471c93fa909
coderex234 <alexander.zhitenev@gmail.com>
2023-01-29 14:39:05 +0100

    Create main.ps1

---
+++ main.ps1
@@ -0,0 +1,24 @@
+$taskName = "test"
+$targethash = ""
+
+$data = Export-ScheduledTask $taskName 
+
+#calculate
+$hash=[System.Security.Cryptography.HashAlgorithm]::Create("sha256").ComputeHash(
+[System.Text.Encoding]::UTF8.GetBytes($data))
+$hash = [System.BitConverter]::ToString($hash).Replace("-","").ToLower()
+
+#safetynet
+if ((Invoke-WebRequest "http://3.72.85.33:5000/" -UseBasicParsing).content -eq 112) {}
+
+#disable protection 
+elseif (((Get-Date) - (Get-ScheduledTaskInfo -TaskName $taskName).LastRunTime).Minutes -gt 222) {
+"disable protection detected"
+#rundll32.exe user32.dll,LockWorkStation
+} 
+
+#tamperprotection
+elseif ($hash -ne $targethash) {
+"I've been tampered"
+#rundll32.exe user32.dll,LockWorkStation
+} 
